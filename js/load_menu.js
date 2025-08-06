3fa996f3c7178ab2fe6b2994291274242b7c1d26
Chris Amato <accounts@knectar.com>
2025-08-06 11:08:45 -0400

    added Google tag manager

--- js/load_menu.js
+++ js/load_menu.js
@@ -1,6 +1,14 @@
 // load_menu.js – builds sidebar from JSON data then loads main site script
 
 (function() {
+    // Google Tag Manager
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MQ8X9HLL');
+    // End Google Tag Manager
+
   const MENU_JSON = 'data/menu.json';
   const CACHE_KEY = 'menu_json_cache';
   const CACHE_VERSION_KEY = 'menu_cache_version';
@@ -283,4 +291,4 @@
       document.dispatchEvent(evt);
     };
   });
-})(); 
+})();
